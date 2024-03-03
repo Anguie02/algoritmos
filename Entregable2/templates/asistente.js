d171f94edf340e28e1792a89fa7094021df83c4c
Anguie02 <1487186@senati.pe>
2024-03-03 00:00:38 -0500

    añadir nombres de productos

--- Entregable2/templates/asistente.js
+++ Entregable2/templates/asistente.js
@@ -1,34 +1,34 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const asistenteContainer = document.getElementById('asistente-container');
-    const asistenteInput = document.getElementById('asistente-input');
-    const asistenteMessages = document.getElementById('asistente-messages');
+  const asistenteContainer = document.getElementById('asistente-container');
+  const asistenteInput = document.getElementById('asistente-input');
+  const asistenteMessages = document.getElementById('asistente-messages');
+  const asistenteForm = document.getElementById('asistente-form');
 
-    // Función para enviar mensajes al servidor
-    function enviarMensajeAsistente() {
-      const mensajeUsuario = asistenteInput.value.trim();
-      if (mensajeUsuario !== '') {
-        // Enviar mensaje al servidor
-        fetch('/enviar_mensaje', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-          },
-          body: `mensaje_usuario=${encodeURIComponent(mensajeUsuario)}`,
-        })
-        .then(response => response.json())
-        .then(data => {
-          // Mostrar la respuesta del asistente
-          asistenteMessages.innerHTML += `<p>Asistente: ${data.respuesta_asistente}</p>`;
-          // Limpiar el campo de entrada
-          asistenteInput.value = '';
-        });
-      }
+  // Función para enviar mensajes al servidor
+  function enviarMensajeAsistente() {
+    const mensajeUsuario = asistenteInput.value.trim();
+    if (mensajeUsuario !== '') {
+      // Enviar mensaje al servidor
+      fetch('/enviar_mensaje', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: `mensaje_usuario=${encodeURIComponent(mensajeUsuario)}`,
+      })
+      .then(response => response.json())
+      .then(data => {
+        // Mostrar la respuesta del asistente
+        asistenteMessages.innerHTML += `<p>Asistente: ${data.respuesta_asistente}</p>`;
+        // Limpiar el campo de entrada
+        asistenteInput.value = '';
+      });
     }
+  }
 
-    // Evento para enviar mensajes cuando se presiona Enter
-    asistenteInput.addEventListener('keydown', function (event) {
-      if (event.key === 'Enter') {
-        enviarMensajeAsistente();
-      }
-    });
+  // Evento para enviar mensajes cuando se envía el formulario
+  asistenteForm.addEventListener('submit', function (event) {
+    event.preventDefault(); // Evitar el envío del formulario por defecto
+    enviarMensajeAsistente();
   });
+});
